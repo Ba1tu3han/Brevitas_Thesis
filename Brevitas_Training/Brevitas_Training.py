88276f1ac14da357385e7f508084261367da9b48
batuhanakkus <batuhanakkus@gmail.com>
2024-03-03 21:49:57 +0200

    added storing accuracy and loss for each epoch by epoch_accuracies = [] epoch_losses = []

--- Brevitas_Training/Brevitas_Training.py
+++ Brevitas_Training/Brevitas_Training.py
@@ -21,6 +21,7 @@ from torch.nn import Module
 import torch.nn.functional as F
 import brevitas.nn as qnn
 
+
 process_start_time = time.time()
 
 #  DOWNLOAD TRAINING AND TEST DATASETS FROM OPEN DATASETS
@@ -112,24 +113,36 @@ def test(dataloader, model, loss_fn):  # testing function
     num_batches = len(dataloader)
     model.eval()
     test_loss, correct = 0, 0
+
     with torch.no_grad():
         for X, y in dataloader:
             X, y = X.to(device), y.to(device)
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
     test_loss /= num_batches
     correct /= size
+    accuracy = 100 * correct
     print(f"Test Error: \n Accuracy (Top1): {(100 * correct):>0.2f}%, Avg loss: {test_loss:>8f} \n")
+    return accuracy, test_loss
 
 
 # TRAINING
 
 epochs = 30
+
+epoch_accuracies = [] # lists to store accuracy and loss for each epoch
+epoch_losses = []
+
 for t in range(epochs):
     print(f"Epoch {t + 1}\n-------------------------------")
     train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
+    accuracy_list, loss_list = test(test_dataloader, model, loss_fn)
+    epoch_accuracies.append(accuracy_list)
+    epoch_losses.append(loss_list)
+    print(epoch_accuracies)
+    print(epoch_losses)
 print("Done!")
 
 
